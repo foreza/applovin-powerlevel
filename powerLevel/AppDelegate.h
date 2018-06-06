1512443270fce2c1186504cbcc9bb4bb418f0f67
Jason C <jforeza@gmail.com>
2018-06-06 10:45:08 -0700

    Initial Commit

---
+++ powerLevel/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  powerLevel
+//
+//  Created by Jason C on 6/6/18.
+//  Copyright © 2018 Jason C. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ powerLevel/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  powerLevel
+//
+//  Created by Jason C on 6/6/18.
+//  Copyright © 2018 Jason C. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
